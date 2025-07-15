4ae4e6a2136284b6e96b097f051bfae73c0e45ce
RaduTeodorProsie <psn.radu@gmail.com>
2025-07-15 15:05:10 +0300

    Added tests

--- src/credential_manager.rs
+++ src/credential_manager.rs
@@ -36,7 +36,7 @@ impl CredentialManager {
     }
 
     fn get_entry<T : IsService>(&self) -> Option<Entry> {
-        Entry::new(&self.username, T::service_name().to_string().as_ref()).ok()
+        Entry::new(T::service_name().to_string().as_ref(), self.username.as_ref()).ok()
     }
     pub fn get<T : IsService>(&self) -> Option<String> {
         self.get_entry::<T>().and_then(|entry| entry.get_password().ok())
@@ -54,4 +54,21 @@ impl CredentialManager {
             .delete_credential()
             .map_err(|err| err.to_string())
     }
+}
+
+mod tests{
+    use super::*;
+    fn add_get_and_delete<T : IsService>(){
+        const DUMMY_VAL: &str = "093c57be61f8785dab30e54632b9896b71ae8d41b930a2b525391b83d5941828";
+        CredentialManager::global().set::<T>(DUMMY_VAL).expect("Error setting credential manager");
+        let stored = CredentialManager::global().get::<T>().expect("Error getting the stored credential");
+        assert_eq!(stored, DUMMY_VAL, "Stored credential doesn't match");
+        CredentialManager::global().delete::<T>().expect("Error deleting credential");
+    }
+    #[test]
+    fn test_services(){
+        add_get_and_delete::<Token>();
+        add_get_and_delete::<Cache>();
+    }
+
 }
