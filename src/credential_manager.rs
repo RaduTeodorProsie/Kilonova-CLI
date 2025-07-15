ac10d9e6e46b6a48a5cb7a0828c76322cdc98629
RaduTeodorProsie <psn.radu@gmail.com>
2025-07-15 14:38:40 +0300

    Generalize the manager

--- src/credential_manager.rs
+++ src/credential_manager.rs
@@ -3,14 +3,31 @@ use std::env;
 use std::sync::OnceLock;
 
 pub struct CredentialManager{
-    service: &'static str,
     username: String,
+}
+
+pub struct Token;
+pub struct Cache;
+pub trait IsService{
+    fn service_name() -> &'static str;
+}
+
+impl IsService for Token{
+    fn service_name() -> &'static str{
+        "kilonova-cli-token"
+    }
+}
+
+impl IsService for Cache{
+    fn service_name() -> &'static str{
+        "kilonova-cli-cache"
+    }
 }
 
 impl CredentialManager {
     pub fn new() -> Self {
         let username = env::var("USER").or_else(|_| env::var("USERNAME")).unwrap(); //you should always have a username
-        Self { service : &"kilonova-cli", username }
+        Self { username }
     }
 
     pub fn global() -> &'static Self {
@@ -18,21 +35,21 @@ impl CredentialManager {
         INSTANCE.get_or_init(|| CredentialManager::new())
     }
 
-    fn get_entry(&self) -> Option<Entry> {
-        Entry::new(&self.username, self.service).ok()
+    fn get_entry<T : IsService>(&self) -> Option<Entry> {
+        Entry::new(&self.username, T::service_name().to_string().as_ref()).ok()
     }
-    pub fn get_token(&self) -> Option<String> {
-        self.get_entry().and_then(|entry| entry.get_password().ok())
+    pub fn get<T : IsService>(&self) -> Option<String> {
+        self.get_entry::<T>().and_then(|entry| entry.get_password().ok())
     }
 
-    pub fn set_token(&self, token: &str) -> Result<(), String> {
-        self.get_entry()
+    pub fn set<T : IsService>(&self, value: &str) -> Result<(), String> {
+        self.get_entry::<T>()
             .ok_or_else(|| "No entry found".to_owned())?
-            .set_password(token)
+            .set_password(value)
             .map_err(|err| err.to_string())
     }
-    pub fn delete_token(&self, target: &str) -> Result<(), String> {
-        self.get_entry()
+    pub fn delete<T : IsService>(&self) -> Result<(), String> {
+        self.get_entry::<T>()
             .ok_or_else(|| "No entry found".to_owned())?
             .delete_credential()
             .map_err(|err| err.to_string())
